93e8aa7746c5e992a41760a772c5a0e4ede8e929
CrazyMarcosMaster <marcoscristaldo21by@gmail.com>
2025-11-18 21:18:55 -0600

    Update script_bienvenida.js

--- js/script_bienvenida.js
+++ js/script_bienvenida.js
@@ -1,11 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let nombre = localStorage.getItem("nombreUsuario");
+document.addEventListener('DOMContentLoaded', () => {
+    const mensaje = document.getElementById('mensajeBienvenida');
+    const nombreUsuario = document.getElementById('nombreUsuario');
 
-    if (!nombre) {
-        nombre = prompt("Ingrese su nombre y apellido:");
-        if (nombre) localStorage.setItem("nombreUsuario", nombre);
+    let nombre = prompt("Por favor, ingresa tu nombre:");
+    let apellido = prompt("Por favor, ingresa tu apellido:");
+
+    if(nombre && apellido){
+        mensaje.textContent = `¡Bienvenido al proyecto, ${nombre} ${apellido}!`;
+        nombreUsuario.textContent = `Hola, ${nombre} ${apellido}`;
+    } else {
+        mensaje.textContent = "Bienvenido al proyecto. Selecciona la API que deseas explorar:";
     }
-
-    document.getElementById("bienvenida").textContent = 
-        "Bienvenido/a " + nombre;
 });
